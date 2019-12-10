827c90f2ae63831b9cdd2a053a61a5488e47f81b
NandoCodeBR <52359881+NandoCodeBR@users.noreply.github.com>
2019-12-10 20:49:54 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -4,18 +4,31 @@
     
     try{
         $conexao = new PDO($host.$database, 'root','');
-        $query = 'create table usuario (
+        $nome = $_GET['nome'];
+        $email = $_GET['email'];
+        /*$query = 'create table usuario (
                 id int not null primary key auto_increment,
                 nome varchar(50) not null,
                 email varchar(100) not null,
                 senha varchar(12) not null)';
         $retorno = $conexao->exec($query);
         echo $retorno;
+         * /
+         */
+        /*
         $query = '
                 INSERT INTO usuario (nome, email, senha) VALUES ("Fernando", "Fernando@uol.com", "Fernando123")
                 ';
         $retorno = $conexao->exec($query);
-        echo $retorno;
+         * 
+        echo $retorno; */
+        $query = 'SELECT nome FROM usuario WHERE nome LIKE"%'."$nome%".'"';
+        $stmt = $conexao->query($query);
+        $list = $stmt->fetch(PDO::FETCH_OBJ); /*use o PDO::FETCH_NUM para passar com os IDS Numéricos
+                                                   PDO::FETCH_ASSOC para passar com os IDS Associativos         */
+        echo "<pre>";
+        echo $list->nome;
+        echo "</pre>";
     } catch (PDOException $ex) {
         echo 'ERRO! Código:'.$ex->getCode(). ' Mensagem do erro: '.$ex->getMessage();
     }
